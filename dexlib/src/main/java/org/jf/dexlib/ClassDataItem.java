ae7235f8dfb6eb5578a7a0fa3199ea4ba8f906de
JesusFreke@JesusFreke.com <JesusFreke@JesusFreke.com@55b6fa8a-2a1e-11de-a435-ffa8d773f76a>
2009-08-11 02:56:41 +0000

    Fixed issue with size calculation in placeValue
    git-svn-id: https://smali.googlecode.com/svn/trunk@384 55b6fa8a-2a1e-11de-a435-ffa8d773f76a

--- dexlib/src/main/java/org/jf/dexlib/ClassDataItem.java
+++ dexlib/src/main/java/org/jf/dexlib/ClassDataItem.java
@@ -119,25 +119,25 @@ public class ClassDataItem extends Item<ClassDataItem> {
 
         EncodedField previousEncodedField = null;
         for (EncodedField encodedField: staticFields) {
-            offset += encodedField.place(offset, previousEncodedField);
+            offset = encodedField.place(offset, previousEncodedField);
             previousEncodedField = encodedField;
         }
 
         previousEncodedField = null;
         for (EncodedField encodedField: instanceFields) {
-            offset += encodedField.place(offset, previousEncodedField);
+            offset = encodedField.place(offset, previousEncodedField);
             previousEncodedField = encodedField;
         }
 
         EncodedMethod previousEncodedMethod = null;
         for (EncodedMethod encodedMethod: directMethods) {
-            offset += encodedMethod.place(offset, previousEncodedMethod);
+            offset = encodedMethod.place(offset, previousEncodedMethod);
             previousEncodedMethod = encodedMethod;
         }
 
         previousEncodedMethod = null;
         for (EncodedMethod encodedMethod: virtualMethods) {
-            offset += encodedMethod.place(offset, previousEncodedMethod);
+            offset = encodedMethod.place(offset, previousEncodedMethod);
             previousEncodedMethod = encodedMethod;
         }
 
@@ -448,7 +448,7 @@ public class ClassDataItem extends Item<ClassDataItem> {
 
             offset += Leb128Utils.unsignedLeb128Size(method.getIndex() - previousIndex);
             offset += Leb128Utils.unsignedLeb128Size(accessFlags);
-            offset += codeItem==null?1:Leb128Utils.unsignedLeb128Size(codeItem.getIndex());
+            offset += codeItem==null?1:Leb128Utils.unsignedLeb128Size(codeItem.getOffset());
             return  offset;
         }
 
