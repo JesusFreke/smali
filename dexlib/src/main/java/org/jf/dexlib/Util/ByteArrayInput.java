7ed253b78bd28abb0a56463ca26bb26c1c07302c
jesusfreke@jesusfreke.com <jesusfreke@jesusfreke.com@55b6fa8a-2a1e-11de-a435-ffa8d773f76a>
2011-05-15 17:06:57 +0000

    Fix bug in ByteArrayInput.readLong
    Thanks to deuns.martinez for reporting this fix
    
    git-svn-id: https://smali.googlecode.com/svn/trunk@808 55b6fa8a-2a1e-11de-a435-ffa8d773f76a

--- dexlib/src/main/java/org/jf/dexlib/Util/ByteArrayInput.java
+++ dexlib/src/main/java/org/jf/dexlib/Util/ByteArrayInput.java
@@ -120,7 +120,7 @@ public class ByteArrayInput
                       ((data[readAt++] & 0xffL) << 32) |
                       ((data[readAt++] & 0xffL) << 40) |
                       ((data[readAt++] & 0xffL) << 48) |
-                      ((data[readAt++] & 0xffL) << 58);
+                      ((data[readAt++] & 0xffL) << 56);
         cursor = readAt;
         return result;
     }
