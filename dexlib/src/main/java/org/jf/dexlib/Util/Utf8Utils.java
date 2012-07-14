66b89545a4b397a7e90dff7f8fff5349c074dcae
Ben Gruver <bgruv@google.com>
2012-07-13 18:27:27 -0700

    Fix bug in Utf8Utils

--- dexlib/src/main/java/org/jf/dexlib/Util/Utf8Utils.java
+++ dexlib/src/main/java/org/jf/dexlib/Util/Utf8Utils.java
@@ -136,7 +136,7 @@ public final class Utf8Utils {
                         return throwBadUtf8(v1, at + 1);
                     }
                     int v2 = bytes[at + 2] & 0xFF;
-                    if ((v1 & 0xc0) != 0x80) {
+                    if ((v2 & 0xc0) != 0x80) {
                         return throwBadUtf8(v2, at + 2);
                     }
                     int value = ((v0 & 0x0f) << 12) | ((v1 & 0x3f) << 6) |
