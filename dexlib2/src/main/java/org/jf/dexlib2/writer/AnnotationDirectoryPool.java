e29142d406fac5efa66d189a06af3401eb03c254
Ben Gruver <bgruv@google.com>
2012-12-29 23:05:54 -0800

    Fix AnnotationDirectoryPool comparisons

--- dexlib2/src/main/java/org/jf/dexlib2/writer/AnnotationDirectoryPool.java
+++ dexlib2/src/main/java/org/jf/dexlib2/writer/AnnotationDirectoryPool.java
@@ -266,7 +266,7 @@ public class AnnotationDirectoryPool {
         public int compareTo(Key o) {
             // compareTo will only be called on keys of the same internability. An internable key will not be compared
             // with a non-internable one.
-            if (hasClassAnnotations()) {
+            if (hasNonClassAnnotations()) {
                 return classDef.getType().compareTo(o.classDef.getType());
             }
             return CollectionUtils.compareAsSet(classDef.getAnnotations(), o.classDef.getAnnotations());
