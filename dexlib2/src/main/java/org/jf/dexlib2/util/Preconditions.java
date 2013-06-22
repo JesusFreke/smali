e636af49311605b8a00488f44fde7f45b78ab905
Ben Gruver <bgruv@google.com>
2013-06-22 13:07:39 -0700

    Fix bound check for short offset in Precoditions class

--- dexlib2/src/main/java/org/jf/dexlib2/util/Preconditions.java
+++ dexlib2/src/main/java/org/jf/dexlib2/util/Preconditions.java
@@ -118,7 +118,7 @@ public class Preconditions {
     }
 
     public static int checkShortCodeOffset(int offset) {
-        if (offset < -32768 || offset > 32768) {
+        if (offset < -32768 || offset > 32767) {
             throw new IllegalArgumentException(
                     String.format("Invalid code offset: %d. Must be between -32768 and 32767, inclusive.", offset));
         }
