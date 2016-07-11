d5c6ffa0595cf5bfe5fcf1054670d9d20de3a253
Ben Gruver <bgruv@google.com>
2016-07-10 20:04:02 -0700

    Move existing smali tests to the org.jf.smali package

--- smali/src/test/java/org/jf/smali/LexerTest.java
+++ smali/src/test/java/org/jf/smali/LexerTest.java
@@ -1,39 +1,38 @@
 /*
- * [The "BSD licence"]
- * Copyright (c) 2010 Ben Gruver
+ * Copyright 2016, Google Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
+ * modification, are permitted provided that the following conditions are
+ * met:
  *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-import org.antlr.runtime.ANTLRInputStream;
+package org.jf.smali;import org.antlr.runtime.ANTLRInputStream;
 import org.antlr.runtime.CommonToken;
 import org.antlr.runtime.CommonTokenStream;
 import org.antlr.runtime.RecognitionException;
-import org.jf.smali.expectedTokensTestGrammarLexer;
-import org.jf.smali.expectedTokensTestGrammarParser;
-import org.jf.smali.smaliFlexLexer;
-import org.jf.smali.smaliParser;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -135,9 +134,9 @@ public class LexerTest {
         String smaliFile = String.format("LexerTest%s%s.smali", File.separatorChar, test);
         String tokensFile = String.format("LexerTest%s%s.tokens", File.separatorChar, test);
 
-        expectedTokensTestGrammarLexer expectedTokensLexer = null;
+        org.jf.smali.expectedTokensTestGrammarLexer expectedTokensLexer = null;
         try {
-            expectedTokensLexer = new expectedTokensTestGrammarLexer(new ANTLRInputStream(
+            expectedTokensLexer = new org.jf.smali.expectedTokensTestGrammarLexer(new ANTLRInputStream(
                     LexerTest.class.getClassLoader().getResourceAsStream(tokensFile)));
         } catch (IOException ex) {
             throw new RuntimeException(ex);
@@ -145,8 +144,8 @@ public class LexerTest {
 
         CommonTokenStream expectedTokensStream = new CommonTokenStream(expectedTokensLexer);
 
-        expectedTokensTestGrammarParser expectedTokensParser =
-                new expectedTokensTestGrammarParser(expectedTokensStream);
+        org.jf.smali.expectedTokensTestGrammarParser expectedTokensParser =
+                new org.jf.smali.expectedTokensTestGrammarParser(expectedTokensStream);
         try {
             expectedTokensParser.top();
         } catch (RecognitionException ex) {
