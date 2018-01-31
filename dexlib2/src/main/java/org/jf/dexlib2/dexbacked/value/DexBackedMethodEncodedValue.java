2215e031bf0824e429a26c95afa712eb7441c20a
Ben Gruver <bgruv@google.com>
2018-01-31 11:47:47 -0800

    Fix typo in DexBackedMethodEncodedValue

--- dexlib2/src/main/java/org/jf/dexlib2/dexbacked/value/DexBackedMethodEncodedValue.java
+++ dexlib2/src/main/java/org/jf/dexlib2/dexbacked/value/DexBackedMethodEncodedValue.java
@@ -41,14 +41,14 @@ import javax.annotation.Nonnull;
 
 public class DexBackedMethodEncodedValue extends BaseMethodEncodedValue {
     @Nonnull public final DexBackedDexFile dexFile;
-    private final int MethodIndex;
+    private final int methodIndex;
 
     public DexBackedMethodEncodedValue(@Nonnull DexReader reader, int valueArg) {
         this.dexFile = reader.dexBuf;
-        MethodIndex = reader.readSizedSmallUint(valueArg + 1);
+        methodIndex = reader.readSizedSmallUint(valueArg + 1);
     }
 
     @Nonnull @Override public MethodReference getValue() {
-        return new DexBackedMethodReference(dexFile, MethodIndex);
+        return new DexBackedMethodReference(dexFile, methodIndex);
     }
 }
