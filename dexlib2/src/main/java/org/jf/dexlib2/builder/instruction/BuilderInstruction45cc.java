8b7f4f015c6fd62286e16c3f981cff935a189e84
aki-ks <aki@kaysubs.de>
2019-07-30 09:03:39 +0200

    Fix precondition check in BuilderInstruction45cc

--- dexlib2/src/main/java/org/jf/dexlib2/builder/instruction/BuilderInstruction45cc.java
+++ dexlib2/src/main/java/org/jf/dexlib2/builder/instruction/BuilderInstruction45cc.java
@@ -63,7 +63,7 @@ public class BuilderInstruction45cc extends BuilderInstruction implements Instru
                                   @Nonnull Reference reference2) {
         super(opcode);
         this.registerCount = Preconditions.check35cAnd45ccRegisterCount(registerCount);
-        this.registerC = (registerCount>0) ? Preconditions.checkNibbleRegister(registerC) : 0;
+        this.registerC = (registerCount>0) ? Preconditions.checkShortRegister(registerC) : 0;
         this.registerD = (registerCount>1) ? Preconditions.checkNibbleRegister(registerD) : 0;
         this.registerE = (registerCount>2) ? Preconditions.checkNibbleRegister(registerE) : 0;
         this.registerF = (registerCount>3) ? Preconditions.checkNibbleRegister(registerF) : 0;
