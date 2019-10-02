539353a85c22f8561907d674ccef6c4b4e0a89da
Ben Gruver <bgruv@google.com>
2019-10-02 16:34:04 -0700

    Use the correct path separator character when looking for classpath entries

--- dexlib2/src/main/java/org/jf/dexlib2/analysis/ClassPathResolver.java
+++ dexlib2/src/main/java/org/jf/dexlib2/analysis/ClassPathResolver.java
@@ -182,7 +182,7 @@ public class ClassPathResolver {
         // It's not a local path, so let's try to resolve it as a device path, relative to one of the provided
         // directories
         List<String> pathComponents = splitDevicePath(entry);
-        Joiner pathJoiner = Joiner.on(File.pathSeparatorChar);
+        Joiner pathJoiner = Joiner.on(File.separatorChar);
 
         for (String directory: classPathDirs) {
             File directoryFile = new File(directory);
