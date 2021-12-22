11f71ae481d0f38dcf8c4198c34135e047dc98c8
Marc Miltenberger <Marc.Miltenberger@sit.fraunhofer.de>
2021-12-22 22:26:26 +0100

    Fix range check for character arrays with elements over 32767

--- dexlib2/src/test/java/org/jf/util/PreconditionsTest.java
+++ dexlib2/src/test/java/org/jf/util/PreconditionsTest.java
@@ -28,12 +28,18 @@ public class PreconditionsTest {
     verifyArrayPayloadElementIsValid(4, Integer.MIN_VALUE);
     verifyArrayPayloadElementIsValid(2, Short.MAX_VALUE);
     verifyArrayPayloadElementIsValid(2, Short.MIN_VALUE);
+    verifyArrayPayloadElementIsValid(2, Character.MAX_VALUE);
+    verifyArrayPayloadElementIsValid(2, Character.MIN_VALUE);
     verifyArrayPayloadElementIsValid(1, Byte.MAX_VALUE);
     verifyArrayPayloadElementIsValid(1, Byte.MIN_VALUE);
 
     verifyArrayPayloadElementIsInvalid(4, ((long) Integer.MAX_VALUE) + 1);
     verifyArrayPayloadElementIsInvalid(4, ((long) Integer.MIN_VALUE) - 1);
-    verifyArrayPayloadElementIsInvalid(2, ((long) Short.MAX_VALUE) + 1);
+    verifyArrayPayloadElementIsInvalid(2, ((long) Short.MIN_VALUE) - 1);
+    //Since short and character have the same size, but different ranges
+    // and cannot be distinguished here, the valid interval is
+    //[Short.MIN_VALUE, Character.MAX_VALUE], i.e. [-32768, 65535]
+    verifyArrayPayloadElementIsInvalid(2, ((long) Character.MAX_VALUE) + 1);
     verifyArrayPayloadElementIsInvalid(2, ((long) Short.MIN_VALUE) - 1);
     verifyArrayPayloadElementIsInvalid(1, ((long) Byte.MAX_VALUE) + 1);
     verifyArrayPayloadElementIsInvalid(1, ((long) Byte.MIN_VALUE) - 1);
